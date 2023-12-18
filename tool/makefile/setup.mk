5d28e72ee28ef7780830baa9bdda4fdaed5b76b2
Plague Fox <plugfox@gmail.com>
2023-12-18 23:45:48 +0400

    Update app icons and launch images

--- tool/makefile/setup.mk
+++ tool/makefile/setup.mk
@@ -2,13 +2,11 @@
 
 # https://pub.dev/packages/flutter_launcher_icons
 icon:
-	@cd example
-	@fvm flutter pub run flutter_launcher_icons -f flutter_launcher_icons.yaml
+	@cd example && dart run flutter_launcher_icons -f flutter_launcher_icons.yaml
 
 # https://pub.dev/packages/flutter_native_splash
 splash:
-	@cd example
-	@fvm flutter pub run flutter_native_splash:create --path=flutter_native_splash.yaml
+	@cd example && dart run flutter_native_splash:create --path=flutter_native_splash.yaml
 
 init-firebase:
 	@cd example
