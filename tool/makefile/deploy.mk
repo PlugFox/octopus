08172d21057d9ce41a462dbc97102d6f03ac2043
Plague Fox <plugfox@gmail.com>
2023-12-22 16:13:15 +0400

    Refactor build and deploy commands in deploy.mk

--- tool/makefile/deploy.mk
+++ tool/makefile/deploy.mk
@@ -1,12 +1,10 @@
 .PHONY: build-web deploy-web serve-web build-android build-windows
 
 build-web:
-	@cd example
-	@fvm flutter build web --release --dart-define-from-file=config/production.json --no-source-maps --pwa-strategy offline-first --web-renderer auto --web-resources-cdn --base-href /
+	@cd example && flutter build web --release --dart-define-from-file=config/production.json --no-source-maps --pwa-strategy offline-first --web-renderer auto --web-resources-cdn --base-href /
 
 deploy-web: build-web
-	@cd example
-	@firebase deploy
+	@cd example && firebase deploy
 
 # https://docs.flutter.dev/platform-integration/web/wasm
 #build-web-wasm:
@@ -16,13 +14,10 @@ deploy-web: build-web
 #	@firebase hosting:channel:deploy wasm --expires 14d
 
 serve-web: build-web
-	@cd example
-	@firebase serve --only hosting -p 8080
+	@cd example && firebase serve --only hosting -p 8080
 
 build-android:
-	@cd example
-	@fvm flutter build apk --release --dart-define-from-file=config/production.json
+	@cd example && flutter build apk --release --dart-define-from-file=config/production.json
 
 build-windows:
-	@cd example
-	@fvm flutter build windows --release --dart-define-from-file=config/production.json
+	@cd example && flutter build windows --release --dart-define-from-file=config/production.json
